27d7402f55cb9d7a3157058d4c4450c9c2c9dcf1
Utkarsh Bahukhandi <utkarshbahukhandi0002@gmail.com>
2021-09-01 17:08:41 +0530

    added like and comment UI

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import {Box,makeStyles, Typography} from '@material-ui/core'
+import React, { useState } from 'react'
+import {Box,makeStyles, TextareaAutosize, Typography} from '@material-ui/core'
 import {Delete, Edit} from '@material-ui/icons'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import DoneIcon from '@material-ui/icons/Done';
 
 const styleClass =makeStyles({
     container:{
@@ -10,23 +12,74 @@ const styleClass =makeStyles({
     commentbox:{
         display:"flex",
         flexDirection:"column"
+    },
+    header:{
+        display:"flex",
+        flexDirection:"row",
+        marginTop:"25px",
+        marginBottom:"5px"
+        
+    },
+    timeicon:{
+        marginLeft:"5px",
+        opacity:"0.5"
+    },
+    date:{
+        marginLeft:"5px",
+        opacity:"0.6"
+    },
+    comment:{
+        marginTop:"5px",
+        marginBottom:"5px",
+        outline:"none",
+        border:"none"
+    },
+    icon:{
+        border:"1px solid #000",
+        borderRadius:"5px",
+        padding:"5px",
+        margin:"5px"
     }
 })
 
 function Comment() {
 
     const styles=styleClass();
+    const [visible,changeVisible]=useState(false);
 
+    const [comment,setComment]=useState("comment");
+    
+    const editComment=(e)=>{
+        setComment(e.target.value);
+    }
+    
     return (
         <Box className={styles.container}>
            <Box className={styles.commentbox}> 
-                <Typography> Author Name</Typography>
-                <Typography> Date </Typography>
-                <Typography>Comment </Typography>
+                <Box className={styles.header}>
+                    <Typography className={styles.author}> Author Name</Typography>
+                    <AccessTimeIcon className ={styles.timeicon}></AccessTimeIcon>
+                    <Typography className={styles.date}>Date</Typography>
+                </Box>
+                {
+                !visible && <Typography className={styles.comment}>{comment}</Typography>
+                }
+                {
+                visible && <TextareaAutosize onChange={(e)=> editComment(e)} className={styles.comment} value={comment}></TextareaAutosize>
+                }
+                
+                
+                
             </Box>    
             <Box className={styles.buttons}>
-                <Edit color="primary" />
-                <Delete color="error" />
+                {
+                !visible && <Edit onClick={()=> changeVisible(!visible)} className={styles.icon} color="primary" />
+                }
+                {
+                visible && <DoneIcon onClick={()=> changeVisible(!visible)} className={styles.icon} color="primary" />
+                }
+
+                <Delete onClick={()=> changeVisible(false)} className={styles.icon} color="error" />
             </Box>    
 
         </Box>    
--- client/src/components/comment/PostComment.jsx
+++ client/src/components/comment/PostComment.jsx
@@ -1,27 +1,42 @@
 import { Box,makeStyles, TextareaAutosize,Button } from '@material-ui/core'
-import React from 'react'
+import React,{useState} from 'react'
 import PersonIcon from '@material-ui/icons/Person';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const styleClass=makeStyles({
     container:{
         display:"flex",
         flexDirection:"row",
         flexWrap: "wrap",
-        alignItems: "flex-start"
+        alignItems: "flex-start",
+        marginTop:"30px"
     },
     commentbox:{
         width:"90%"
+    },
+    like:{
+        marginTop:"20px"
     }
 })
 
 
 function PostComment() {
     const styles=styleClass();
+    const [visible,changeVisible]=useState(false);
     return (
-        <Box className={styles.container}>
-            <PersonIcon/>
-            <TextareaAutosize className={styles.commentbox} minRows={3} />
-            <Button variant="contained" color="primary"> Post</Button>
+        <Box>
+            {
+                !visible && <FavoriteBorderIcon className={styles.like} onClick={()=>{changeVisible(!visible)}} color="error"/>
+            }
+            {
+                visible && <FavoriteIcon className={styles.like} onClick={()=>{changeVisible(!visible)}}  color="error"/>
+            }
+            <Box className={styles.container}>
+                <PersonIcon/>
+                <TextareaAutosize className={styles.commentbox} minRows={3} />
+                <Button variant="contained" color="primary"> Post</Button>
+            </Box>
         </Box>
 
     )
